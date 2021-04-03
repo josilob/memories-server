b7f36b60695731494208e871ae0c6b8ca9b29e43
josilob <bjosilo@gmail.com>
2021-04-02 20:12:20 -0400

    server setup and running

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-import express from 'express';
-// old syntax is replaced: const express = require('express')
-// all that is needed is: "main": "index.js" line inside of package.json
-// import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+require('dotenv').config();
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+const { USERNAME, PASSWORD } = process.env;
+const CONNECTION_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.mk9uk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 // newer way, without warnings >4.16 express version
-app.use(body.json({ limit: '30mb', extended: true }));
-app.use(app.urlencoded({ limit: '30mb', extended: true }));
+// app.use(body.json({ limit: '30mb', extended: true }));
+// app.use(app.urlencoded({ limit: '30mb', extended: true }));
 // older way without bodyParser out of the box
-// app.use(bodyParser.json({ limit: '30mb', extended: true }));
-// app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const 
+const PORT = process.env.PORT || 5000;
+
+mongoose
+	.connect(CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+	.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false); // no warning in console
