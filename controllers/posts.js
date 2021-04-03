69d3bfd22dee2eea40d4eaf3dd42f4cd82075b87
josilob <bjosilo@gmail.com>
2021-04-02 20:36:11 -0400

    import/export controllers

---
+++ controllers/posts.js
@@ -0,0 +1,5 @@
+const getPosts = (req, res) => {
+	res.send('THIS WORKS!');
+};
+
+exports.getPosts = getPosts;
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const posts = require('../controllers/posts.js');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.send('THIS WORKS');
-});
+router.get('/', posts.getPosts);
 
 module.exports = router;
