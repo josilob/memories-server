792728a45db18650408eb9182dee7c070981ac66
josilob <bjosilo@gmail.com>
2021-05-10 22:17:23 -0400

    likes count update w/ auth

--- controllers/posts.js
+++ controllers/posts.js
@@ -64,6 +64,8 @@ export const updatePost = async (req, res) => {
 export const deletePost = async (req, res) => {
 	const { id } = req.params;
 
+	if (!req.userId) return res.json({ message: 'Unauthenticated' });
+
 	if (!mongoose.Types.ObjectId.isValid(id))
 		return res.status(404).send(`No post with id: ${id}`);
 
@@ -80,11 +82,17 @@ export const likePost = async (req, res) => {
 
 	const post = await PostMessage.findById(id);
 
-	const updatedPost = await PostMessage.findByIdAndUpdate(
-		id,
-		{ likeCount: post.likeCount + 1 },
-		{ new: true }
-	);
+	const index = post.likes.findIndex((id) => id === String(req.userId));
+
+	if (index === -1) {
+		post.likes.push(res.userId);
+	} else {
+		post.likes = post.likes.filter((id) => id !== String(req.userId));
+	}
+
+	const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+		new: true,
+	});
 
 	res.json(updatedPost);
 };
