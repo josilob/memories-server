b25b2b3040657f1c5431de774dea4dabe22300a4
josilob <bjosilo@gmail.com>
2021-05-10 22:00:57 -0400

    signin and signup controllers

--- controllers/user.js
+++ controllers/user.js
@@ -32,4 +32,31 @@ export const signin = async (req, res) => {
 	}
 };
 
-export const signup = async (req, res) => {};
+export const signup = async (req, res) => {
+	const { email, password, confirmPassword, firstName, lastName } = req.body;
+	try {
+		const existingUser = await User.findOne({ email });
+
+		if (existingUser)
+			return res.status(400).json({ message: 'User already exist.' });
+
+		if (password !== confirmPassword)
+			return res.status(400).json({ message: 'Passwords do not match' });
+
+		const hashedPassword = await bcrypt.hash(password, 12); //12 => level of hash dificulty, 12 is standard
+
+		const result = await User.create({
+			email,
+			password: hashedPassword,
+			name: `${firstName} ${lastName}`,
+		});
+
+		const token = jwt.sign({ email: result.email, id: result._id }, 'test', {
+			expiresIn: '1h',
+		});
+
+		res.status(200).json({ result, token });
+	} catch (error) {
+		res.status(500).json({ message: 'Something went wrong.' });
+	}
+};
